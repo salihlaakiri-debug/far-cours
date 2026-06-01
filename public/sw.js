642cc760faf20a5dea268513cf9b615d2de71a38
salihlaakiri-debug <salihlaakiri@gmail.com>
2026-06-01 22:30:13 +0100

    fix: proper square PWA icons for Android install prompt

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE = "far-cache-v1";
-const STATIC = ["/ERB.png", "/ERB-original.png"];
+const STATIC = ["/ERB.png", "/ERB-original.png", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
